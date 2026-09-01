f7fa6ed1d5020d82d8139baeea0de8c05a4f6bf3
Fabrizio Quintini <fa.quintini@duocuc.cl>
2026-09-01 02:44:22 -0400

    Se agrega estructura inicial del home y productos

---
+++ js/productos.js
@@ -0,0 +1,47 @@
+const productos = [
+    {
+        codigo: "FR001",
+        nombre: "Manzanas Fuji",
+        categoria: "Frutas Frescas"
+    },
+    {
+        codigo: "FR002",
+        nombre: "Naranjas Valencia",
+        categoria: "Frutas Frescas"
+    },
+    {
+        codigo: "FR003",
+        nombre: "Plátanos Cavendish",
+        categoria: "Frutas Frescas"
+    },
+    {
+        codigo: "VR001",
+        nombre: "Zanahorias Orgánicas",
+        categoria: "Verduras Orgánicas"
+    },
+    {
+        codigo: "VR002",
+        nombre: "Espinacas Frescas",
+        categoria: "Verduras Orgánicas"
+    },
+    {
+        codigo: "VR003",
+        nombre: "Pimientos Tricolores",
+        categoria: "Verduras Orgánicas"
+    },
+    {
+        codigo: "PO001",
+        nombre: "Miel Orgánica",
+        categoria: "Productos Orgánicos"
+    },
+    {
+        codigo: "PO003",
+        nombre: "Quinua Orgánica",
+        categoria: "Productos Orgánicos"
+    },
+    {
+        codigo: "PL001",
+        nombre: "Leche Entera",
+        categoria: "Productos Lácteos"
+    }
+];
